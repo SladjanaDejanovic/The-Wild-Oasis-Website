7ed1fd369e2ad6b83b7647c1a70bfa64fb750e22
Sladjana <csl.bunny.2207@gmail.com>
2024-09-12 21:25:17 +0200

    optimizing img

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,11 @@
-import Logo from "./_components/Logo";
-import Navigation from "./_components/Navigation";
+import Header from "./_components/Header";
+import "@/app/_styles/globals.css";
 
 import { Josefin_Sans } from "next/font/google";
 const josefin = Josefin_Sans({
 	subsets: ["latin"],
 	display: "swap",
 });
-
-console.log(josefin);
-
-import "@/app/_styles/globals.css";
 
 export const metadata = {
 	title: {
@@ -24,13 +20,12 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body
-				className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+				className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
 			>
-				<header>
-					<Logo />
-				</header>
-				<Navigation />
-				<main>{children}</main>
+				<Header />
+				<div className="flex-1 px-8 py-12">
+					<main className="max-w-7xl mx-auto">{children}</main>
+				</div>
 			</body>
 		</html>
 	);
