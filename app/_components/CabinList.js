1b27ba66ad3763ae5752268212eb64cdd4dc34c6
Sladjana <csl.bunny.2207@gmail.com>
2024-09-24 23:31:24 +0200

    filter

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -3,15 +3,33 @@ import { getCabins } from "@/app/_lib/data-service";
 // import { revalidatePath } from "next/cache";
 import { unstable_noStore as noStore } from "next/cache";
 
-async function CabinList() {
+async function CabinList({ filter }) {
 	noStore();
 	const cabins = await getCabins();
 	// revalidatePath("/cabins"); // changed data in supabse doesn't show, so to get new data, use revalidatePath (but this prevents static rendering)
 	if (!cabins.length) return null;
 
+	let displayedCabins;
+	if (filter === "all") displayedCabins = cabins;
+
+	if (filter === "small")
+		displayedCabins = cabins.filter(
+			(cabin) => cabin.maxCapacity <= 3
+		);
+
+	if (filter === "medium")
+		displayedCabins = cabins.filter(
+			(cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7
+		);
+
+	if (filter === "large")
+		displayedCabins = cabins.filter(
+			(cabin) => cabin.maxCapacity <= 8
+		);
+
 	return (
 		<div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-			{cabins.map((cabin) => (
+			{displayedCabins.map((cabin) => (
 				<CabinCard cabin={cabin} key={cabin.id} />
 			))}
 		</div>
